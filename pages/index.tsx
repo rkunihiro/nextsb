798901877dfb11646840ce62b5a220aec5b0ab60
Ryoh Kunihiro <11649545+rkunihiro@users.noreply.github.com>
2020-04-09 21:37:58 +0900

    Convert to TypeScript project

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+const Home = () => (
+    <>
+        <Head>
+            <title>Home</title>
+        </Head>
+        <div>Hello</div>
+    </>
+);
+
+export default Home;

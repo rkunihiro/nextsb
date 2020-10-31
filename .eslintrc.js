463b33080b13ebb79b16781bf124c0946c0bcd50
Ryoh Kunihiro <11649545+rkunihiro@users.noreply.github.com>
2020-10-31 09:22:22 +0900

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true,
+    },
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/eslint-recommended", "plugin:@typescript-eslint/recommended", "plugin:react/recommended"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: "./tsconfig.json",
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+    },
+    plugins: ["@typescript-eslint", "react"],
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+    rules: {
+        "react/prop-types": "off",
+    },
+};
